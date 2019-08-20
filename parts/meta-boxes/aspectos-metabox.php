b43c0924ffd7bf27306daa4a49735b610c3749c3
Federico <bicho44@gmail.com>
2019-08-20 17:02:35 -0300

    Actualización del tipo de editor para los Aspectos e Itinerario

--- parts/meta-boxes/aspectos-metabox.php
+++ parts/meta-boxes/aspectos-metabox.php
@@ -9,8 +9,27 @@ Author: Federico Reinoso
 Author email: admin@imgdigital.com.ar
 */
 
+// piklist('field', array(
+//     'type' => 'editor',
+//     'field' => 'imgd_aspectos_importantes_field',
+//     'label' => __('Datos a tener en cuenta en esta excursi&oacute;n', 'imgd')
+// ));
+
 piklist('field', array(
     'type' => 'editor',
-    'field' => 'imgd_aspectos_importantes_field',
-    'label' => __('Datos a tener en cuenta en esta excursi&oacute;n', 'imgd')
-));
+    'field' => 'imgd_aspectos_importantes_field', 
+    'label' => __('Datos a tener en cuenta en esta excursi&oacute;n', 'imgd'),
+    'options' => array( // Pass any option that is accepted by wp_editor()
+      'wpautop' => true,
+      'media_buttons' => true,
+      'shortcode_buttons' => true,
+      'teeny' => false,
+      'dfw' => false,
+      'quicktags' => true,
+      'drag_drop_upload' => true,
+      'tinymce' => array(
+        'resize' => false,
+        'wp_autoresize_on' => true
+      )
+    )
+ ));
--- parts/meta-boxes/itinerario-metabox.php
+++ parts/meta-boxes/itinerario-metabox.php
@@ -9,8 +9,28 @@ Author: Federico Reinoso
 Author email: admin@imgdigital.com.ar
 */
 
+// piklist('field', array(
+//     'type' => 'editor',
+//     'field' => 'imgd_itinerario_field',
+//     'label' => __('Itinerario de la excursi&oacute;n', 'imgd')
+// ));
+
+
 piklist('field', array(
     'type' => 'editor',
     'field' => 'imgd_itinerario_field',
-    'label' => __('Itinerario de la excursi&oacute;n', 'imgd')
-));
+    'label' => __('Itinerario de la excursi&oacute;n', 'imgd'),
+    'options' => array( // Pass any option that is accepted by wp_editor()
+      'wpautop' => true,
+      'media_buttons' => true,
+      'shortcode_buttons' => true,
+      'teeny' => false,
+      'dfw' => false,
+      'quicktags' => true,
+      'drag_drop_upload' => true,
+      'tinymce' => array(
+        'resize' => false,
+        'wp_autoresize_on' => true
+      )
+    )
+ ));
