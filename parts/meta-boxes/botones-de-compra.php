9c5993ed93d58e5ab1b16b14d61800953b3ccd9b
Federico <bicho44@gmail.com>
2019-09-06 19:55:02 -0300

    botones de compra primer intento

---
+++ parts/meta-boxes/botones-de-compra.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Title: Link de Compra
+Post Type: imgd_actividad
+Description: Campo para el link de compra
+Priority: high
+Order: 3
+Author: Federico Reinoso
+Author email: admin@imgdigital.com.ar
+*/
+
+// piklist('field', array(
+//     'type' => 'editor',
+//     'field' => 'imgd_aspectos_importantes_field',
+//     'label' => __('Datos a tener en cuenta en esta excursi&oacute;n', 'imgd')
+// ));
+
+piklist('field', array(
+  'type' => 'text',
+  'field' => 'imgd_texto_link_compra_field',
+  'label' => __('Texto a mostrar en el botón para acceder al link', 'imgd'),
+));
+
+piklist('field', array(
+    'type' => 'text',
+    'field' => 'imgd_link_compra_field',
+    'label' => __('Link de Compra y / o Mercado Pago', 'imgd'),
+ ));
