faf6700d78c293bb1efca83fce74ea31f5d37422
Federico <bicho44@gmail.com>
2019-09-06 23:57:43 -0300

    agregado de las settings correcpondientes al CPT pertenecen al plug-in que las define y no al tema

--- parts/settings/dash-settings.php
+++ parts/settings/dash-settings.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Title: Home Page Settings
-Setting: xopciones_imgd
+Setting: opciones_imgd
 Order: 10
 Tab: Home
 Flow: IMGD Settings
@@ -131,3 +131,157 @@ foreach ($registered_post_types as $post_type => $value)
     , 'placeholder'=>__('[ISW id=XXXX]', 'imgd')
     )
 ))));*/
+
+/* Noticias Destacadas */
+
+piklist (
+    'field',
+    array(
+        'type' => 'radio',
+        'scope' => 'post_meta',
+        'field' => 'imgd_destaca',
+        'label' => __('Mostar Línea de Destacados en la Home Page', 'imgd'),
+        'value' => 0,
+        'attributes' => array(
+            'class' => 'radio'
+        ),
+        'choices' => array(
+            0 => __('No', 'imgd'),
+            1 => __('Si', 'imgd')
+        ),
+        'position' => 'wrap'
+    )
+);
+
+piklist(
+    'field',
+    array(
+        'type' => 'select'
+        ,'field' => 'imgd_desta_cant'
+        ,'description' => __('Cuantas Noticias Destacadas', 'imgd')
+        ,'value' => '6'
+        ,'label' => __('Cantidad', 'imgd')
+        , 'conditions' => array(
+                    array(
+                        'field' => 'imgd_destaca'
+                        , 'value' => 1
+                    )
+                )
+        ,'attributes' => array(
+                'class' => 'small-text'
+        )
+        , 'choices' => array(
+            2 => 2,
+            3 => 3,
+            4 => 4,
+            6 => 6
+        )
+    )
+);
+
+/* Línea de Actividades */
+piklist (
+    'field',
+    array(
+        'type' => 'radio',
+        'scope' => 'post_meta',
+        'field' => 'imgd_actividades',
+        'label' => __('Mostar Línea de actividades en la Home Page', 'imgd'),
+        'value' => 0,
+        'attributes' => array(
+            'class' => 'radio'
+        ),
+        'choices' => array(
+            0 => __('No', 'imgd'),
+            1 => __('Si', 'imgd')
+        ),
+        'position' => 'wrap'
+    )
+);
+
+piklist(
+    'field',
+    array(
+        'type' => 'select'
+        ,'field' => 'imgd_actividades_cant'
+        ,'description' => __('Cuantas Actividades Mostrar', 'imgd')
+        ,'value' => '6'
+        ,'label' => __('Cantidad', 'imgd')
+        , 'conditions' => array(
+                    array(
+                        'field' => 'imgd_actividades'
+                        , 'value' => 1
+                    )
+                )
+        ,'attributes' => array(
+                'class' => 'small-text'
+        )
+        , 'choices' => array(
+            2 => 2,
+            3 => 3,
+            4 => 4,
+            6 => 6,
+            0 => 'Todas'
+        )
+    )
+);
+
+/*
+piklist (
+    'field',
+    array(
+        'type' => 'radio',
+        'scope' => 'post_meta',
+        'field' => 'imgd_columnas',
+        'label' => __('Mostar Línea de Columnas', 'imgd'),
+        'value' => 0,
+        'attributes' => array(
+            'class' => 'radio'
+        ),
+        'choices' => array(
+            0 => __('No', 'imgd'),
+            1 => __('Si', 'imgd')
+        ),
+        'position' => 'wrap'
+    )
+);
+*/
+
+piklist (
+    'field',
+    array(
+        'type' => 'radio',
+        'scope' => 'post_meta',
+        'field' => 'imgd_goto_top',
+        'label' => __('Muestra el GotoTop?', 'imgd'),
+        'value' => 0,
+        'attributes' => array(
+            'class' => 'radio'
+        ),
+        'choices' => array(
+            0 => __('No', 'imgd'),
+            1 => __('Si', 'imgd')
+        ),
+        'position' => 'wrap'
+    )
+);
+
+piklist('field', array(
+    'type' => 'file'
+    ,'field' => 'imgd_image_to_top'
+    ,'label' => __('Imagen del Goto top', 'imgd')
+    ,'options' => array(
+      'basic' => true
+    )
+    , 'conditions' => array(
+                    array(
+                        'field' => 'imgd_goto_top'
+                        , 'value' => 1
+                    )
+                )
+  ));
+
+/*
+* @todo: Image Field
+* @todo: Image Size?
+*/
